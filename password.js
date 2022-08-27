5fdf3760bff37ac4404323e2368e3912560367fd
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-08-26 20:50:27 -0500

    password basics finished

--- password.js
+++ password.js
@@ -1,5 +1,26 @@
 let password = "RubberDuck2";
 
-if (password.length >= 10 && password) {
+let alphabet = "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let hasLetter = false;
+for(let i = 0; i < password.length; i++) {
+    if (alphabet.indexOf(password[i]) > 0) {
+        hasLetter = true;
+        break;
+    }
+}
+
+let numbers = "0123456789";
+let hasNumber = false;
+for(let i = 0; i < password.length; i++) {
+    if (numbers.indexOf(password[i]) > 0) {
+        hasNumber = true;
+        break;
+    }
+}
+
+// Alternate way: This is from some major Googling.  ---> if (password.length >= 10 && /[a-zA-Z]/.test(password) && /[0-9]/.test(password)) {
+if (password.length >= 10 && hasLetter && hasNumber) {
     console.log("Success!");
+} else {
+    console.log("Failure!");
 }
